a47b8edfbc6a9b288a09c5296eb93a4aa9db8c9a
1560485_auctionwebnodejs <37243411+1560485-auctionwebnodejs@users.noreply.github.com>
2018-04-16 22:10:21 +0700

    Add files via upload

---
+++ src/routes/registrationRoute.js
@@ -0,0 +1,35 @@
+var express=require('express');  
+var mongodb=require('mongodb').MongoClient;  
+var usersRouter=express.Router();  
+var u_router=function(navMenu){  
+    usersRouter.route("/")  
+        .get(function(req,res){  
+                res.render('register', {  
+                    title:'Registration',  
+                    menu:navMenu,  
+                   message:'' 
+                });  
+        })  
+       
+        .post(function(req,res){  
+            console.log(req.body);
+            var url='mongodb://localhost:27017/NodeDemoWebApp';  
+            mongodb.connect(url,function(err,db){  
+                var collection=db.collection('Users');  
+                collection.insertOne(req.body,function(err,result){  
+                    res.render('register', {  
+                        title:'Registration',  
+                        menu:navMenu,  
+                        message:'Successfully Registered' 
+                   
+                    });  
+                   
+                });  
+               
+            });  
+          
+        });  
+        return usersRouter;  
+       
+}     
+module.exports=u_router;  
